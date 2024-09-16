83ead5f12a4df3cc7c46312beebe5bfbdd52266e
Andrewbana <satoruloveg@gmail.com>
2024-09-16 04:29:19 +0000

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@
 //NOTE : cara menjadi publik owner silahkan Pergi ke file; all/database/owner.json
 
 require("./all/module")
-global.owner = "27632461796,27765359269" 
-global.namaCreator = "" 
+global.owner = "233549772750" 
+global.namaCreator = "King Rio✨" 
 global.autoJoin = false 
 global.antilink = true 
 global.versisc = '1.0' 
